4982567a8055529f1ca46f71fabfdae1457b1815
watercolor-bluejayy <footballgirl78000@gmail.com>
2024-02-06 21:14:25 -0500

    template entered, starting original code now

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,61 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+//written by Jayy Wattman on 2/6/2024
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+void printMenu() {
+    cout << "Please Select which operation to perform:" << endl;
+    cout << "\t1. Factorial" << endl;
+    cout << "\t2. Arithmetic Series" << endl;
+    cout << "\t3. Geometric Series" << endl;
+    cout << "\t4. Exit" << endl;
+    cout << "Your Selection: " << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void factorial() {
+    //write factorial code here
+    cout << "Factorial:" << endl;
+    cout << "Enter a number: ";
+    cin >> n
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void arithmetic() {
+    //write arithmetic series code here
+}
+
+void geometric() {
+    //write geometric series code here
+}
+
+int main() {
+    int choice;
+    char again;
+
+    do {
+        printMenu();
+        cin << choice;
+
+        //Quit if user chooses to exit (or any invalid choice)
+        if (choice > 3 || choice < 1) {
+            return 0;
+        }
+
+        else if (choice == 1) {
+            factorial();
+        }
+        else if (choice == 2) {
+            arithmetic();
+        }
+        else if (choice == 3) {
+            geometric();
+        }
+
+        cout << "Go Again? [Yes/No] ";
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y')
+}
+
