30de1d18c4003b3a8be1dee19d0b504febd8dcbb
watercolor-bluejayy <footballgirl78000@gmail.com>
2024-02-07 01:18:48 -0500

    factorial complete!

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,14 +13,33 @@ void printMenu() {
     cout << "\t3. Geometric Series" << endl;
     cout << "\t4. Exit" << endl;
     cout << "Your Selection: " << endl;
+  
 }
 
 void factorial() {
     //write factorial code here
+    int n;
+    int result = 1;
     cout << "Factorial:" << endl;
     cout << "Enter a number: ";
-    cin >> n
-}
+    cin >> n;
+    
+    while (n < 0) {
+        cout << "Nope, please enter a POSITIVE number this time...: ";
+        cin >> n;
+    }
+
+    cout << n << "! = ";
+
+    for (int a = 1; a <= n; a++) {
+        result = result * a;
+        cout << a;
+        if (a < n) {
+            cout << "*";
+        }
+    }
+    cout << "= " << result << endl;
+ }
 
 void arithmetic() {
     //write arithmetic series code here
@@ -36,7 +55,7 @@ int main() {
 
     do {
         printMenu();
-        cin << choice;
+        cin >> choice;
 
         //Quit if user chooses to exit (or any invalid choice)
         if (choice > 3 || choice < 1) {
@@ -53,9 +72,9 @@ int main() {
             geometric();
         }
 
-        cout << "Go Again? [Yes/No] ";
+        cout << "Go Again? [Y/N] ";
         cin >> again;
 
-    } while (again == 'y' || again == 'Y')
+    } while (again == 'y' || again == 'Y');
 }
 
